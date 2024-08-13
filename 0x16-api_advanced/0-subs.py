bbc0e69e50bb3f257aafafeefdcbcc72de6013d0
Mahi <mahlet7000@gmail.com>
2024-08-13 07:02:03 -0700

    api advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,12 +9,12 @@ import requests
 def number_of_subscribers(subreddit):
     """Returns the nubmer of subscribers or 0"""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {'User-Agent': 'User Agent 1.0'}
+    headers = {'User-Agent': 'UserAgent/1.0'}
     response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code == 200:
-        data = response.json()
-        subscribers = data.get("data").get("subscribers")
+        data = response.json().get("data")
+        subscribers = data.get("subscribers")
         return subscribers
-
-    return 0
+    else:
+        return 0
--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -7,15 +7,15 @@ import requests
 
 def top_ten(subreddit):
     """Titles of the first 10 hot posts listed for a given subreddit."""
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
     headers = {'User-Agent': 'TopTen/1.0'}
     response = requests.get(url, headers=headers, allow_redirects=False)
-    
+
     if response.status_code == 200:
         data = response.get('data')
         children = data.get('children')
-        
-        for i in range(10):
-            print(children[i].get('data').get('title'))
+
+        for child in children:
+            print(child.get('data').get('title'))
     else:
         print("None")
